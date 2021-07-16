7d4ceeac93e79f386c6d9b4a127a8834a22f78d4
kyu <kyu.fastai@gmail.com>
2021-07-16 15:30:51 -0500

    for, range, functions

--- main.go
+++ main.go
@@ -1,6 +1,10 @@
 package main
 
-func main() {
-	name := "kyu"
+import "fmt"
 
+func main() {
+	name := "Kyu ! ! ! ! ! ! ! Is my name"
+	for _, letter := range name {
+		fmt.Printf("%x\n", letter)
+	}
 }
