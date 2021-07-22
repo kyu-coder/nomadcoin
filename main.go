d78ef62d96ed19b8cb928429621c00b3c197d702
kyu <kyu.fastai@gmail.com>
2021-07-22 11:46:32 -0500

    add block

--- main.go
+++ main.go
@@ -28,10 +28,24 @@ func home(rw http.ResponseWriter, r *http.Request) {
 
 }
 
+func add(rw http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case "GET":
+		templates.ExecuteTemplate(rw, "add", nil)
+	case "POST":
+		r.ParseForm()
+		data := r.Form.Get("blockData")
+		blockchain.GetBlockchain().AddBlock(data)
+		http.Redirect(rw, r, "/home", http.StatusPermanentRedirect)
+	}
+
+}
+
 func main() {
 	templates = template.Must(template.ParseGlob(templateDir + "pages/*.gohtml"))
 	templates = template.Must(templates.ParseGlob(templateDir + "partials/*.gohtml"))
 	http.HandleFunc("/", home)
+	http.HandleFunc("/add", add)
 	fmt.Printf("Listening on http://localhost%s\n", port)
 	log.Fatal(http.ListenAndServe(port, nil))
 }
