e9b89c1f370db568d8974039a1d2295fc07d0d2e
Kyu L <68824087+kyu-coder@users.noreply.github.com>
2022-11-04 01:01:52 -0500

    mine block - no timer yet

--- main.go
+++ main.go
@@ -1,22 +1,7 @@
 package main
 
-import (
-	"crypto/sha256"
-	"fmt"
-	"strings"
-)
+import "github.com/kyu-coder/nomadcoin/cli"
 
 func main() {
-	difficulty := 4
-	target := strings.Repeat("0", difficulty)
-	nonce := 1
-	for {
-		hash := fmt.Sprintf("%x", sha256.Sum256([]byte("hello"+fmt.Sprint(nonce))))
-		fmt.Printf("Hash:%s\nTarget:%s\nNonce:%d\n\n", hash, target, nonce)
-		if strings.HasPrefix(hash, target) {
-			return
-		} else {
-			nonce++
-		}
-	}
+	cli.Start()
 }
